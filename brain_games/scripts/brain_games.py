d841e978e6ad4f0833f21afad61ae9985c1edfe0
Настюшик <veterinastya@gmail.com>
2023-01-17 22:55:24 +0100

    welcoming user function added to cli.py and brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
 
 def main():
-	print('Welcome to the Brain Games!')
-if __name__ == '__main__':
+    welcome_user()
+
+
+if __name__ == "__main__":
     main()
