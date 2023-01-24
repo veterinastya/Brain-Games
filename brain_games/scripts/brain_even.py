ad6dcd935ccf06b3f1f9a8e2064be6089edd5f03
Настюшик <veterinastya@gmail.com>
2023-01-24 18:58:49 +0100

    fixing brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -7,16 +7,20 @@ number = random.randint(1, 100)
 
 def main():
     even_start()
-
+# проверяем число на четность путем деления его на 2 без остатка
 def is_even(number):
     if (number % 2) == 0:
         return 'yes'
     else:
         return 'no'
-
+# запуск игры
 def even_start():
+    #приветствуем пользователя, узнаем его имя
+
     print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name?')
+
+    # улучшить потом код, чтобы нельзя было оставить поле пустым!
     print(f'Hello, {user_name}!')
     print(RULES)
     i = 0
@@ -25,12 +29,15 @@ def even_start():
         correct_answer = is_even(question)
         print('Question: ' + str(question))
         user_answer = input('Your answer:')
+        #добавить сюда запрет на введение чего-либо, помимо йес или ноу
         if str(correct_answer) == user_answer:
             print('Correct!')
             i += 1
             continue
         else:
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, " + user_name + "!")
+            print(
+            f"'{user_answer}' is wrong answer ;(. "
+            f"Correct answer was '{correct_answer}'.")
             return
     print('Congratulations, ' + user_name + '!')
 
