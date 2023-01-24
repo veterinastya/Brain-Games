c863e957d5ae1294a958b574c86900c5b41ed14c
Настюшик <veterinastya@gmail.com>
2023-01-24 18:41:15 +0100

    fixing brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,6 +5,9 @@ import prompt
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 number = random.randint(1, 100)
 
+def main():
+    even_start()
+
 def is_even(number):
     if (number % 2) == 0:
         return 'yes'
@@ -12,32 +15,24 @@ def is_even(number):
         return 'no'
 
 def even_start():
-    correct_answer = is_even(number)
-    question = number
-    return question, correct_answer
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name?')
+    print(f'Hello, {user_name}!')
     print(RULES)
     i = 0
     while i < 3:
-        print('Question: {question}')
+        question = random.randint(1, 100)
+        correct_answer = is_even(question)
+        print('Question: ' + str(question))
         user_answer = input('Your answer:')
         if str(correct_answer) == user_answer:
             print('Correct!')
+            i += 1
+            continue
         else:
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, {user_name}!")
-        i += 1
-    print('Congratulations, {user_name}!')
-
-def greeting():
-    print('Welcome to the Brain Games!')
-
-def welcome_user():
-    user_name = prompt.string('May I have your name?')
-    print(f'Hello, {user_name}!')
-
-def main():
-    greeting()
-    welcome_user()
-    even_start()
+            print("'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, " + user_name + "!")
+            return
+    print('Congratulations, ' + user_name + '!')
 
 if __name__ == '__main__':
     main()
