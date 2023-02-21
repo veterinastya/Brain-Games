01b442f089c7e6e22aa53e5204062af5bbc95cb4
Настюшик <veterinastya@gmail.com>
2023-02-22 00:17:13 +0100

    fixing progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -18,15 +18,18 @@ def game_data():
     start = randint(MIN_START, MAX_START)
     # the step between numbers in this progression
     step = randint(MIN_STEP, MAX_STEP)
+    correct_answer, question = create_progression(start, list_length, step)
+    return question, correct_answer
+
+
+def create_progression(start, list_length, step):
     progression_list = []  # creating a list to add progression
     stop = start + step * list_length  # determination of ending point
     for value in range(start, stop, step):  # creating progression
         progression_list.append(value)
-    missing_index = randint(0, list_length - 1)  # determing hidden number
+    missing_index = randint(1, list_length - 1)  # determing hidden number
     missing_number = progression_list[missing_index]
     progression_str = ' '.join(map(str, progression_list))
     # hiding the number
     progression = progression_str.replace(str(missing_number), '..')
-    question = progression
-    correct_answer = str(missing_number)
-    return question, correct_answer
+    return missing_number, progression
