729f18ecd4cd1e5105daaf5671ccb76c4c67bc9c
Настюшик <veterinastya@gmail.com>
2023-02-21 12:08:25 +0100

    adding comments, editind pyproject.toml and reamde.md

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -8,7 +8,7 @@ RULES = 'Find the greatest common divisor of given numbers.'
 def game_data():
     num1 = randint(1, 100)
     num2 = randint(1, 100)
-    question = num1, num2
+    question = str(num1) + ' ' + str(num2)
     correct_answer = math.gcd(num1, num2)
     return question, correct_answer
     
