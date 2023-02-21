ce245eba2663dc1135cb3717ae5594e3a806afcc
Настюшик <veterinastya@gmail.com>
2023-02-21 23:30:41 +0100

    fixing progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -14,7 +14,7 @@ def game_data():
     stop = start + step * list_length  # determination of ending point
     for value in range(start, stop, step):  # creatingprogression
         progression_list.append(value)
-    missing_index = randint(1, list_length - 1)  # determing hidden number
+    missing_index = randint(0, list_length - 1)  # determing hidden number
     missing_number = progression_list[missing_index]
     progression_str = ' '.join(map(str, progression_list))
     # hiding the number
