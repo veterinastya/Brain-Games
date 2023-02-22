2743fe9b895d0b6be5554ddbbe97c422a96ca125
Настюшик <veterinastya@gmail.com>
2023-02-22 01:07:22 +0100

    fixing code for codeclimate

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,11 +1,10 @@
 import random
-from random import randint
 
 # in this game user needs to guess if given number is even or not
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 MIN_NUMBER = 1
 MAX_NUMBER = 100
-number = random.randint(MIN_NUMBER, MAX_NUMBER)
+is_even_number = random.randint(MIN_NUMBER, MAX_NUMBER)
 
 
 def is_even(number):
@@ -15,10 +14,9 @@ def is_even(number):
 
 
 def game_data():
-    number = randint(MIN_NUMBER, MAX_NUMBER)
-    question = number
+    question = is_even_number
     correct_answer = is_even(question)
-    if is_even(number):
+    if is_even(is_even_number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
